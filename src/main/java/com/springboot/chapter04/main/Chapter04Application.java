bbee182f62f190b187caec4c4eef8a0e31773952
rockwang <rockwang@localhost.localdomain>
2019-02-06 10:14:57 +0800

    init from start.spring.io

---
+++ src/main/java/com/springboot/chapter04/main/Chapter04Application.java
@@ -0,0 +1,14 @@
+package com.springboot.chapter04.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter04Application.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/springboot/chapter04/main/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.springboot.chapter04.main;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Chapter04Application.class);
+	}
+
+}
+
